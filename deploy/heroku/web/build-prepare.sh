ca048835f05b98432555b44575b23060fecae573
thoarrow <thoarrown@gmail.com>
2021-08-08 15:28:45 +0700

    remove require file .env

--- deploy/heroku/web/build-prepare.sh
+++ deploy/heroku/web/build-prepare.sh
@@ -5,7 +5,7 @@ echo "* Prepare for build Web *"
 echo "*************************"
 echo
 
-ENV_FILE_PATH="./apps/client/.env"
+# ENV_FILE_PATH="./apps/client/.env"
 
 # Config NEXTAUTH_URL server
 LOCAL_URL="http:\/\/localhost:3001"
@@ -19,11 +19,11 @@ else
 fi
 
 echo "--> Config NEXTAUTH_URL server: ${NEXTAUTH_URL}"
-sed -i "s|${LOCAL_URL}|${NEXTAUTH_URL}|" "${ENV_FILE_PATH}"
-if [[ "${?}" -ne 0 ]]; then
-  echo "--> Error: Edit config ${ENV_FILE_PATH} not success (NEXTAUTH_URL)."
-  exit 1
-fi
+# sed -i "s|${LOCAL_URL}|${NEXTAUTH_URL}|" "${ENV_FILE_PATH}"
+# if [[ "${?}" -ne 0 ]]; then
+#   echo "--> Error: Edit config ${ENV_FILE_PATH} not success (NEXTAUTH_URL)."
+#   exit 1
+# fi
 
 # Config mode
 DEV_MODE='development'
@@ -37,20 +37,20 @@ else
 fi
 
 echo "--> Config application mode: ${SELECT_MODE}"
-sed -i "s/${DEV_MODE}/${SELECT_MODE}/" "${ENV_FILE_PATH}"
-if [[ "${?}" -ne 0 ]]; then
-  echo "--> Error: Edit config ${ENV_FILE_PATH} not success (MODE)."
-  exit 1
-fi
+# sed -i "s/${DEV_MODE}/${SELECT_MODE}/" "${ENV_FILE_PATH}"
+# if [[ "${?}" -ne 0 ]]; then
+#   echo "--> Error: Edit config ${ENV_FILE_PATH} not success (MODE)."
+#   exit 1
+# fi
 
 # Config JWT secret
 JWT_SECRET=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c32)
 
 echo "--> Config JWT secret: ${JWT_SECRET}"
-sed -i "s/secret/${JWT_SECRET}/" "${ENV_FILE_PATH}"
-if [[ "${?}" -ne 0 ]]; then
-  echo "--> Error: Edit config ${ENV_FILE_PATH} not success (JWT_SECRET)."
-  exit 1
-fi
+# sed -i "s/secret/${JWT_SECRET}/" "${ENV_FILE_PATH}"
+# if [[ "${?}" -ne 0 ]]; then
+#   echo "--> Error: Edit config ${ENV_FILE_PATH} not success (JWT_SECRET)."
+#   exit 1
+# fi
 
 exit 0
