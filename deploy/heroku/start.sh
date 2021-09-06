dcb6972fa9b1e3d121df23c2032c746e8a5df1c9
thoarrow <thoarrown@gmail.com>
2021-09-06 16:36:06 +0700

    Fix heroku start

--- deploy/heroku/start.sh
+++ deploy/heroku/start.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 
-# Prepare for start Web
-bash ./deploy/heroku/web/start-prepare.sh
-if [[ "${?}" -ne 0 ]]; then
-  exit 1
-fi
+# # Prepare for start Web
+# bash ./deploy/heroku/web/start-prepare.sh
+# if [[ "${?}" -ne 0 ]]; then
+#   exit 1
+# fi
+
+# # Start
+# node dist/apps/api/main & yarn nx run client:serve:production
+
+# exit 0
+
 
 # Start
-node dist/apps/api/main & yarn nx run client:serve:production
+log "Starting..."
+node dist/apps/api/main & next start dist/apps/web -p ${PORT}
 
-exit 0
+log "Done"
